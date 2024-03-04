aa7fd0f71581a80847f939e62b954d29a609a8e7
unknown <areyad203011@bscse.uiu.ac.bd>
2024-03-04 10:57:40 +0600

    update css

--- js/app.js
+++ js/app.js
@@ -32,9 +32,9 @@ const fetchAll = (postCategory) => {
                             allPost.push(element);
                         }
                         const cardDiv = document.createElement('div');
-                        cardDiv.className = 'bg-base-200 p-10 rounded-2xl flex flex-col lg:flex-row border-2 w-full gap-4 mb-8';
+                        cardDiv.className = 'bg-base-200 p-8 rounded-2xl flex flex-col lg:flex-row border-2 w-full gap-4 mb-8';
                         cardDiv.innerHTML = ` 
-                            <div class=" px-4  relative ">
+                            <div class=" px-4 relative ">
                             <img src="${element.image}" alt="" class="lg:w-16 rounded-lg ">
 
                                 <div class="absolute top-[-5px]  lg:top-[-6px] right-5">
@@ -45,12 +45,12 @@ const fetchAll = (postCategory) => {
                             </div>
 
                             <div class=" flex-grow">
-                                <span># ${element.category}</span>
-                                <span class="ml-6">Author : ${element.author?.name}</span>
-                                <p class="font-bold">${element.title}</p>
-                                <p class="pb-4 border-b-2 border-dashed inter">${element.description}</p>
-                                <div class="flex justify-between ">
-                                    <div class=" flex gap-4">
+                                <span class="font-medium text-sm"># ${element.category}</span>
+                                <span class="ml-6 font-medium text-sm">Author : ${element.author?.name}</span>
+                                <p class="font-bold text-xl">${element.title}</p>
+                                <p class="pb-4 border-b-2 border-dashed inter border-gray-300 text-[#12132D99]">${element.description}</p>
+                                <div class="flex justify-between mt-4">
+                                    <div class=" flex gap-4 text-[#12132D99]">
                                         <p class="text[#12132D99]"><i class="fa-regular fa-message"></i> ${element.comment_count}</p>
                                         <p><i class="fa-regular fa-eye"></i> ${element.view_count}</p>
                                         <p><i class="fa-regular fa-clock"></i> ${element.posted_time} min</p>
@@ -97,17 +97,16 @@ const fetchLatestPosts = () => {
                             class="rounded-xl" />
                     </figure>
                     <div class="card-body ">
-                        <p><i class="fa-regular fa-calendar"></i> <span id="date"> ${element.author.posted_date || "No publish date"}</span></p>
-                        <h2 class="card-title" id="latest-title">${element.title}</h2>
-                        <p id="latest-description">${element.description}</p>
+                        <p class="text-[#12132D99]"><i class="fa-regular fa-calendar"></i> <span id="date"> ${element.author.posted_date || "No publish date"}</span></p>
+                        <h2 class="card-title " id="latest-title">${element.title}</h2>
+                        <p id="latest-description" class="text-[#12132D99]">${element.description}</p>
                         <div class="card-actions">
-                            <div>
+                            <div class="mr-2">
                                 <img id="latest-profile_img" src="${element.profile_image}" alt="" class="w-12 rounded-full">
-
                             </div>
                             <div>
-                                <p id="latest-author">${element.author.name}</p>
-                                <p id="latest-desgination">${element.author.designation || "Unknown"}</p>
+                                <p id="latest-author" class="font-bold">${element.author.name}</p>
+                                <p id="latest-desgination" class="text-[#12132D99]">${element.author.designation || "Unknown"}</p>
                             </div>
                         </div>
                     </div>`
@@ -123,9 +122,9 @@ function readCount(id) {
             const bookmarkedPost = document.createElement('div');
             count++;
             readCountP.innerText = count;
-            bookmarkedPost.className = 'bg-white px-4 flex justify-between rounded-lg items-center lg:text-sm mb-4';
-            bookmarkedPost.innerHTML = `<p class="">${element.title}</p>
-                <div class="flex items-center gap-1"><i class="fa-regular fa-eye"></i> ${element.view_count}</div>`;
+            bookmarkedPost.className = 'bg-white px-4 py-2 flex justify-between rounded-lg items-center lg:text-sm mb-4';
+            bookmarkedPost.innerHTML = `<p class="font-bold">${element.title}</p>
+                <div class="flex items-center gap-1 text-[#12132D99]"><i class="fa-regular fa-eye"></i> ${element.view_count}</div>`;
             selectForRead.appendChild(bookmarkedPost);
 
         }
